8012f21f5fde09cda92057761d8dd9453971c420
AnnaSnoeijs <anna@snoeijs.tech>
2024-10-20 09:58:28 +0000

    Synced to anna-pynq

---
+++ applications/iictest/main.d
@@ -0,0 +1,9 @@
+main.o: main.c ../..//library/libpynq.h ../..//library/adc.h \
+ ../..//library/arm_shared_memory_system.h ../..//library/audio.h \
+ ../..//library/buttons.h ../..//library/gpio.h ../..//library/pinmap.h \
+ ../..//library/display.h ../..//library/fontx.h ../..//library/i2cps.h \
+ ../..//library/iic.h ../..//library/interrupt.h ../..//library/leds.h \
+ ../..//library/log.h ../..//library/pwm.h ../..//library/switchbox.h \
+ ../..//library/uart.h ../..//library/uio.h ../..//library/util.h \
+ ../..//library/version.h ../..//platform/lcdconfig.h \
+ ../..//platform/platform.h
