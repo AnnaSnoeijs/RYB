198efdf7a58e9620ced97e5c703d07b9ac36d5ce
AnnaSnoeijs <anna@snoeijs.tech>
2024-10-24 14:11:52 +0000

    Synced to anna-pynq

--- applications/pull.sh
+++ applications/pull.sh
@@ -19,8 +19,17 @@ updatedifs() {
 		 done
 }
 
+cd ~/git/RYB/applications
 echo Pulling git
 git pull
 updatedifs "~/git/RYB/applications/  ~/libpynq-5EWC0-2023-v0.2.6/applications/"
+
+# recompile everything
+rm ~/libpynq-5EWC0-2023-v0.2.6/applications/*/main.d
+rm ~/libpynq-5EWC0-2023-v0.2.6/applications/*/main.o
+for i in $(ls ~/libpynq-5EWC0-2023-v0.2.6/applications/); do
+	cd ~/libpynq-5EWC0-2023-v0.2.6/applications/$i && make
+done;
+cd ~/git/RYB/applications
 
 IFS=$OLDIFS
